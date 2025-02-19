bafabe690953cd86f5e65c06f178509d8b6a93ff
Purva-25 <purvamhase0@gmail.com>
2025-02-18 17:03:15 -1200

    Frontend folder commited

---
+++ Frontend/Face_Matching/src/component/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Heading = () => {
+  return (
+    <h1 style={{ textAlign: "center", color: "#333", marginTop: "20px" }}>
+      Welcome to My React App
+    </h1>
+  );
+};
+
+export default Heading;
